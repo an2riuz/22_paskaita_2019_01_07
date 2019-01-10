a95daad00acdb0cecc821e5164a27deb0f2a51d9
an2riuz <an2riuz@gmail.com>
2019-01-10 20:02:31 +0200

    Preadedu testu rasyma.

---
+++ TodApp/TodoAppTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TodoAppTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TodoAppTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4136987c-d9de-475e-8ef9-5d1d5a240568")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TodApp/TodoAppTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TodApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TodoAppTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TodoItem test = new TodoItem();
+            int a = 9;
+            int b = 5;
+
+
+            Assert.AreEqual(a + b, 14, "a + b ne 14");
+        }
+    }
+}
